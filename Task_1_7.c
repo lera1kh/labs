468df961b95809c5542c4a6f8cf932c598593c18
lera1kh <lera.khrushkova@mail.ru>
2023-01-24 14:26:52 +0300

    Update Task_1_7.c

--- Task_1_7.c
+++ Task_1_7.c
@@ -1,44 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <limits.h>
 
 int main()
 {
-    srand(time(NULL));
-    int size;
-    printf("Size of array: ");
-    scanf("%d", &size);
-    if (size <= 0) {
-        printf("Try again,incorrect input\n");
+    int size = 5;
+    int arr[size];
+    for (int i = 0; i < size; i++){
+        arr[i] = rand() % 1000;
+        printf("%d ", arr[i] );
+    }
+    printf("\n");
 
-        return 0;
+    int max_el = INT_MIN;
+    int ind_max;
+    int min_el = INT_MAX;
+    int ind_min;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] > max_el){
+            max_el = arr[i];
+            ind_max = i;
+        }
+        if (arr[i] < min_el){
+            min_el = arr[i];
+            ind_min = i;
+        }
     }
-    int* arr = (int*)malloc(size * sizeof(int));
-    int i;
-    for (i = 0; i < size; i++) {
-        arr[i] = -1000 + rand() % 2000;
-    }
-    printf("Array:\n");
-    for (i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
-    int max_el = 0, min_el = 0;
-    for (i = 1; i < size; i++) {
-        if (arr[max_el] < arr[i]) max_el = i;
-        if (arr[min_el] > arr[i]) min_el = i;
-    }
-    printf("Minimum element %d\n", arr[min_el]);
-    printf("Maximum element %d\n", arr[max_el]);
     int tmp;
-    tmp = arr[max_el];
-    arr[max_el] = arr[min_el];
-    arr[min_el] = tmp;
-    printf("New array:\n");
-    for (i = 0; i < size; i++) {
-        printf("%d ", arr[i]);
+    tmp = arr[ind_max];
+    arr[ind_max] = arr[ind_min];
+    arr[ind_min] = tmp;
+
+    for (int i = 0; i < size; i++){
+        printf("%d ", arr[i] );
     }
     printf("\n");
-    free(arr);
     return 0;
 }
