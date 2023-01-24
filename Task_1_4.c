b6d80472f5c6b8820024da15e0794aa0573dc7fa
lera1kh <lera.khrushkova@mail.ru>
2023-01-24 14:26:47 +0300

    Update Task_1_4.c

--- Task_1_4.c
+++ Task_1_4.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]){
     FILE *f_in, *f_out;
-    char str1[100], str2[100], str3[100];
-    if (argc != 2) {
+    char str1[100],str2[100],str3[100];
+    if (argc != 2){
         printf("Enter filename\n");
-        return 0;
+        return 1;
+    
     }
-
     f_in = fopen(argv[1], "r");
-    if (!f_in) {
-        printf("Problems with files\n");
-        return 0;
-    }
-    f_out = fopen("newtxt.txt", "w");
+        if (!f_in) {
+            printf("Problems with files\n");
+            return 0;
+        }
+    f_out = fopen("new.txt", "w");
 
     while (fscanf(f_in, "%s%s%s", str1, str2, str3) != EOF) {
         fprintf(f_out, "%s %s %s\n", str3, str1, str2);
@@ -22,6 +22,6 @@ int main(int argc, char* argv[]) {
     fclose(f_in);
     fclose(f_out);
     remove(argv[1]);
-    rename("newtxt.txt", argv[1]);
+    rename("new.txt", argv[1]);
     return 0;
 }
